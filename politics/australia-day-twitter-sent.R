e130bceddd298d7981b37aa229349bb9bc77c4f7
Trent Henderson <trenthenderson@Trents-MacBook-Pro-2.local>
2021-01-20 11:07:36 +1000

    Added InvasionDay to twitter scraper

--- politics/australia-day-twitter-sent.R
+++ politics/australia-day-twitter-sent.R
@@ -16,9 +16,40 @@ source("twitter-auth/twitter-creds.R") # Not pushed to git due to privacy
 
 #----------------------- Extract tweets ----------------------------
 
-tweets <- searchTwitter("#AustraliaDay", n = 3200, lang = "en")
-tweets_df <- twListToDF(tweets) # Convert to data frame
+#' Function to pull tweets based on a specific hashtag
+#' 
+#' @param hashtag the hashtag to filter the Twitter API for
+#' @return a dataframe of scraped tweets
+#' @author Trent Henderson
+#' 
+
+pull_tweets <- function(hashtag){
+  
+  if(!grepl("#", hashtag)){
+    stop("hashtag should have a # at the front and be written as one word.")
+  }
+  
+  tweets <- searchTwitter(hashtag, n = 3200, lang = "en")
+  
+  # Convert to dataframe and add some useful flags as columns
+  
+  df <- twListToDF(tweets) %>%
+    mutate(hashtag = hashtag) %>%
+    mutate(flag = case_when(
+           grepl("#AustraliaDay", text)                               ~ "#AustraliaDay only",
+           grepl("#InvasionDay", text)                                ~ "#InvasionDay only",
+           grepl("#AustraliaDay", text) & grepl("#InvasionDay", text) ~ "Both #AustraliaDay and #InvasionDay"))
+  
+  return(df)
+}
+
+ausday <- pull_tweets("#AustraliaDay")
+invday <- pull_tweets("#InvasionDay")
+
+d <- bind_rows(ausday, invday)
 
 #----------------------- High level analysis -----------------------
+
+#
 
 
